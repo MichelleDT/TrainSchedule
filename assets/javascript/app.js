e80efab211db7cd33dce0d9b935567a687d7543a
Michelle <michelle@vlan-2715-10-17-217-93.guest.wireless.sydney.edu.au>
2019-09-12 19:03:59 +1000

    fixed html and fxed error in database connection. App now works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,86 +1,84 @@
-var config = {
-	apiKey: "AIzaSyDts6Yhgaby6XqloH96wqm_8RZfcFP_Kv4",
-	authDomain: "hifi-51014.firebaseapp.com",
-	databaseURL: "https://hifi-51014.firebaseio.com",
-	projectId: "hifi-51014",
-	storageBucket: "hifi-51014.appspot.com",
-	messagingSenderId: "199206818561",
-	appId: "1:199206818561:web:6fade7bcdfb0c7f5bd4100"
-  };
+  var firebaseConfig = {
+		apiKey: "AIzaSyBVGrcxHrcmaI1pm2-0bAFHwH4xETsbfI4",
+		authDomain: "train-schedule-1d6ca.firebaseapp.com",
+		databaseURL: "https://train-schedule-1d6ca.firebaseio.com",
+		projectId: "train-schedule-1d6ca",
+		storageBucket: "",
+		messagingSenderId: "769607346079",
+		appId: "1:769607346079:web:48e546e0cb986cc4483faa"
+};
 
-  // apiKey: "AIzaSyASyaPm8uh9R0qa8dDolAIjrE5Rn10rVt8",
-  //authDomain: "click-counter-f806a.firebaseapp.com",
-  //databaseURL: "https://click-counter-f806a.firebaseio.com",
-  //projectId: "click-counter-f806a",
-  //storageBucket: "click-counter-f806a.appspot.com",
-  
-  firebase.initializeApp(config);
-  var database = firebase.database();
-  var tableBody = $("tbody");
-  
-  var submit = $("#submit");
-  
-  submit.on("click", function(e) {
-	e.preventDefault();
-	var name = $("#name").val();
-	var destination = $("#destination").val();
-	var first = $("#first").val();
-	var frequency = $("#frequency").val();
-  
-	database.ref().push({
-	  name: name,
-	  destination: destination,
-	  first: first,
-	  frequency: frequency
-	});
-  });
-  
-  database.ref().on("child_added", function(snap) {
-	var firstTrainTime = moment(first, "hh:mm");
-	var timeDifference = moment().diff(moment(firstTrainTime), "minutes");
-	var timeSinceLastDeparture = timeDifference % frequency;
-	var minutesAway = frequency - timeSinceLastDeparture;
-	var nextArrival = moment()
-	  .add(minutesAway, "minutes")
-	  .format("HH:mm");
-  
-	var data = snap.val();
-	name = data.name;
-	destination = data.destination;
-	first = data.first;
-	frequency = data.frequency;
-	var id = snap.key;
-	var results =
-	  "<tr data-id='" +
-	  id +
-	  "'><td>" +
-	  name +
-	  "</td><td>" +
-	  destination +
-	  "</td><td>" +
-	  first +
-	  "</td><td>" +
-	  frequency +
-	  "</td><td>" +
-	  nextArrival +
-	  "</td><td>" +
-	  parseInt(minutesAway) +
-	  "</td><td><button class='btn btn-danger' id='delete'>Delete</button></td></tr>";
-	tableBody.prepend(results);
-	$("#delete").on("click", function(e) {
-	  e.preventDefault();
-	  var td = $(this).parent();
-	  var tr = td.parent();
-	  var id = tr.attr("data-id");
-	  database.ref(id).remove();
-	  tr.remove();
-	});
-	clearValues();
-  });
-  
-  function clearValues() {
-	$("#name").val("");
-	$("#destination").val("");
-	$("#first").val("");
-	$("#frequency").val("");
-  }
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+function presentTimeHeader() {
+    var time = moment().format("HH:mm:ss");
+    $("#presentTime").text(time);
+    setTimeout(presentTimeHeader, 1000);
+}
+
+database.ref().on("child_added", function(snapshot) {
+
+    console.log(snapshot.val());
+
+    newTrainName = snapshot.val().trainName;
+    newDestination = snapshot.val().destination;
+    newFrequency = snapshot.val().frequency;
+    newFirstTrainTime = snapshot.val().firstTrainTime;
+
+    var newFirstTrainTimeConverted = moment(newFirstTrainTime, "hh:mm")
+    console.log(newFirstTrainTimeConverted);
+
+    var currentTime = moment().format("HH:mm:ss");
+    console.log("current time: " + currentTime);
+
+    var timeDifference = moment().diff(moment(newFirstTrainTimeConverted), "minutes");
+    // console.log("difference between times: " + timeDifference);
+
+    var timeSinceLastDeparture = timeDifference % newFrequency;
+    console.log("minutes since last departure: " + timeSinceLastDeparture);
+
+    var minutesAway = newFrequency - timeSinceLastDeparture
+    console.log("minutes until next departure: " + minutesAway);
+
+    var nextArrival = moment().add(minutesAway, "minutes").format("HH:mm")
+
+    var trainRow =
+        $("<tr><td>" + newTrainName + "</td>" +
+            "<td>" + newDestination + "</td>" +
+            "<td>" + newFrequency + "</td>" +
+            "<td>" + nextArrival + "</td>" +
+            "<td>" + minutesAway + "</td></tr>");
+
+    $("#trainScheduleTable").append(trainRow);
+
+});
+
+// // Submit Button
+$("#submit").on("click", function(event) {
+
+
+    event.preventDefault();
+
+    // Input values
+    var trainNameInput = $("#trainName").val().trim();
+    var destinationInput = $("#destination").val().trim();
+    var frequencyInput = $("#frequency").val().trim();
+    var firstTrainTimeInput = $("#firstTrainTime").val().trim();
+
+    console.log(trainNameInput);
+    console.log(destinationInput);
+    console.log(frequencyInput);
+    console.log(firstTrainTimeInput);
+
+    database.ref().push({
+        trainName: trainNameInput,
+        destination: destinationInput,
+        frequency: frequencyInput,
+        firstTrainTime: firstTrainTimeInput,
+    });
+
+});
+
+presentTimeHeader();
